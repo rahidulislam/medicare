a78cdd4c4f6f5abfcbe0b4c77b52993d6ee7462f
Rahidul Islam <33887458+rahidulislam@users.noreply.github.com>
2021-12-23 11:48:10 +0600

    banner is dynamiced
    Banner content comes from database

--- core/views.py
+++ core/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View
+from core.models import Banner
 # Create your views here.
 
 class HomeView(View):
     template_name = 'core/index.html'
     
     def get(self, request):
-        return render(request, self.template_name)
+        banners = Banner.objects.all().order_by('-id')[:5]
+        doctors = Banner.objects.all()
+        context = {
+            'banners': banners,
+            'doctors': doctors
+        }
+        return render(request, self.template_name, context)
